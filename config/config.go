a3a783016531d29b6597cc38e4205eabda33cc5a
yang.yu <yang.yu@ihandysoft.com>
2019-05-11 17:12:32 +0800

    xxx

---
+++ config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+			"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+const FILE_NAME = "config.json"
+
+type Config struct {
+	Name string `json:"name"`
+	User string `json:"user"`
+	Password string `json:"password"`
+}
+
+
+
+var config = new(Config)
+func init() {
+	fmt.Println("init config...")
+	content, err := ioutil.ReadFile(FILE_NAME)
+	if err != nil {
+		log.Fatal("can't find config.json")
+	}
+	if err := json.Unmarshal(content, config); err == nil {
+		fmt.Println("json -> struct")
+		fmt.Println(config)
+	} else {
+		fmt.Println(err)
+	}
+}
+
+func Get() *Config {
+	return config
+}
---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"gowiki/config"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	filename = "data/" + filename
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func LoadPage(title string) (*Page, error) {
+	filename := "data/" + title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{title, body}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := LoadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := LoadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			fmt.Println("xxxxxxxx")
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
+
+func GeSync(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"code": 200}`))
+}
+func GeTest(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte("test ansible"))
+}
+
+func Books(w http.ResponseWriter, req *http.Request) {
+	config := config.Get()
+	w.Write([]byte(config.Password + config.User))
+}
+func main() {
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/ge/sync", GeSync)
+	http.HandleFunc("/test/ansible", GeTest)
+	http.HandleFunc("/books", Books)
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
